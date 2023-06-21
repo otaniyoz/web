65104744a54d4025909a05ba887cbd8636c7d3ca
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-06-21 11:50:53 +0600

    added pocket puzzles to projects

---
+++ src/routes/projects/pocket-puzzles/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import Heading from "$lib/components/Heading.svelte";
+  import Paragraph from "$lib/components/Paragraph.svelte";
+</script>
+
+<svelte:head>
+  <title>Pocket puzzles: a collection of puzzles to fit in a pocket</title>
+</svelte:head>
+
+<section class="page-section">
+  <article>
+    <Heading
+      headingTitle="Pocket puzzles: a collection of puzzles to fit in a pocket"
+      headingLevel={2}
+    />
+    <img
+      alt="A screen of an app showing sliding tiles puzzle in the unsolved state"
+      class="grid-col14"
+      src="../../pocket-puzzles/light-screenshot.png"
+    />
+    <img
+      alt="A screen of an app showing sliding tiles puzzle in the unsolved state"
+      class="grid-col14"
+      src="../../pocket-puzzles/dark-screenshot.png"
+    />
+    <Paragraph
+      paragraphClass="grid-col14"
+      paragraphContent="Available online at https://otaniyoz.github.io/pocket-puzzles/"
+    />
+  </article>
+</section>
+
+<style>
+  img {
+    height: 100%;
+    max-width: 100%;
+    aspect-ratio: auto;
+  }
+
+  article {
+    display: grid;
+    gap: var(--gap-small);
+    row-gap: var(--gap-regular);
+    grid-area: auto / 1 / auto / 5;
+  }
+
+  @media screen and (min-width: 1200px) {
+    article {
+      grid-area: 1 / 1 / auto / 5;
+    }
+  }
+</style>
