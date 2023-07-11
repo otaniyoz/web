fe12754c58bf8d819750b9060ce1474d54dd0e75
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-07-11 11:19:22 +0600

    updated barrel project page

--- src/routes/projects/oil-barrel/+page.svelte
+++ src/routes/projects/oil-barrel/+page.svelte
@@ -2,7 +2,5 @@
   <title>Concept design of landing page for a barrel of oil</title>
 </svelte:head>
 
-<section>
-  <img alt="Landing page light theme" src="../../oil-barrel/1.jpg" />
-  <img alt="Landing page dark theme" src="../../oil-barrel/0.jpg" />
-</section>
+<img alt="Landing page light theme" src="../../oil-barrel/1.jpg" />
+<img alt="Landing page dark theme" src="../../oil-barrel/0.jpg" />
