203d351ae899e03b35212735bc150b103944aaf0
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-07-11 11:13:05 +0600

    updated genuary 2023 project page

--- src/routes/projects/genuary-2023/+page.svelte
+++ src/routes/projects/genuary-2023/+page.svelte
@@ -7,39 +7,47 @@
 </svelte:head>
 
 <section>
-  <CardsGrid
-    cardsMaps={[
-      { video: "../../genuary-2023/1.mp4" },
-      { image: "../../genuary-2023/2.png" },
-      { video: "../../genuary-2023/3.mp4" },
-      { video: "../../genuary-2023/4.mp4" },
-      { image: "../../genuary-2023/5.png" },
-      { image: "../../genuary-2023/6.png" },
-      { image: "../../genuary-2023/7.png" },
-      { video: "../../genuary-2023/8.mp4" },
-      { image: "../../genuary-2023/9.png" },
-      { video: "../../genuary-2023/10.mp4" },
-      { image: "../../genuary-2023/11.png" },
-      { image: "../../genuary-2023/12.png" },
-      { video: "../../genuary-2023/13.mp4" },
-      { image: "../../genuary-2023/14.png" },
-      { video: "../../genuary-2023/15.mp4" },
-      { image: "../../genuary-2023/16.png" },
-      { video: "../../genuary-2023/17.mp4" },
-      { video: "../../genuary-2023/18.mp4" },
-      { image: "../../genuary-2023/19.png" },
-      { image: "../../genuary-2023/20.png" },
-      { image: "../../genuary-2023/21.png" },
-      { video: "../../genuary-2023/22.mp4" },
-      { video: "../../genuary-2023/23.mp4" },
-      { image: "../../genuary-2023/24.png" },
-      { image: "../../genuary-2023/25.png" },
-      { image: "../../genuary-2023/26.png" },
-      { video: "../../genuary-2023/27.mp4" },
-      { video: "../../genuary-2023/28.mp4" },
-      { image: "../../genuary-2023/29.png" },
-      { video: "../../genuary-2023/30.mp4" },
-      { video: "../../genuary-2023/31.mp4" },
-    ]}
-  />
+  <article>
+    <CardsGrid
+      cardsMaps={[
+        { video: "../../genuary-2023/1.mp4" },
+        { image: "../../genuary-2023/2.png" },
+        { video: "../../genuary-2023/3.mp4" },
+        { video: "../../genuary-2023/4.mp4" },
+        { image: "../../genuary-2023/5.png" },
+        { image: "../../genuary-2023/6.png" },
+        { image: "../../genuary-2023/7.png" },
+        { video: "../../genuary-2023/8.mp4" },
+        { image: "../../genuary-2023/9.png" },
+        { video: "../../genuary-2023/10.mp4" },
+        { image: "../../genuary-2023/11.png" },
+        { image: "../../genuary-2023/12.png" },
+        { video: "../../genuary-2023/13.mp4" },
+        { image: "../../genuary-2023/14.png" },
+        { video: "../../genuary-2023/15.mp4" },
+        { image: "../../genuary-2023/16.png" },
+        { video: "../../genuary-2023/17.mp4" },
+        { video: "../../genuary-2023/18.mp4" },
+        { image: "../../genuary-2023/19.png" },
+        { image: "../../genuary-2023/20.png" },
+        { image: "../../genuary-2023/21.png" },
+        { video: "../../genuary-2023/22.mp4" },
+        { video: "../../genuary-2023/23.mp4" },
+        { image: "../../genuary-2023/24.png" },
+        { image: "../../genuary-2023/25.png" },
+        { image: "../../genuary-2023/26.png" },
+        { video: "../../genuary-2023/27.mp4" },
+        { video: "../../genuary-2023/28.mp4" },
+        { image: "../../genuary-2023/29.png" },
+        { video: "../../genuary-2023/30.mp4" },
+        { video: "../../genuary-2023/31.mp4" },
+      ]}
+    />
+  </article>
 </section>
+
+<style>
+  article {
+    grid-area: auto / 1 / auto / -1;
+  }
+</style>
