c1799ad319653c4d376abeb8c1c4fb0552aabde9
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-08-16 11:46:49 +0600

    etudes projects: added link to code

--- src/routes/projects/etudes/+page.svelte
+++ src/routes/projects/etudes/+page.svelte
@@ -15,8 +15,8 @@
       title="Etudes in code"
       level={1}
       useInline={false}
-      noteTitle={["See GIFs on Telegram"]}
-      noteLink={["https://t.me/etudesincode"]}
+      noteTitle={["See GIFs on Telegram", "See code on Github"]}
+      noteLink={["https://t.me/etudesincode", "https://github.com/otaniyoz/etudes"]}
     />
     <Paragraph
       paragraphContent="Etudes are small pieces of code I write sometimes. Here are some of them:"
