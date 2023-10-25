3ad15b7e1bc9f8fc4d39eab00277fcb4d94b4a47
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-10-25 20:00:43 +0600

    duh

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
   import typographString from "../../utils/typographString";
-  import { base } from "$app/paths";
 
   export let href: string;
   export let text: string;
+  export let target = "_blank";
+  export let externalLink: boolean;
 
   if (text && text.length) {
     text = typographString(text);
   }
 </script>
 
-{#if href.startsWith(base)}
-  <a href={href}>{text}</a>
+{#if externalLink}
+  <a {href} {target}>{text}</a>↗
 {:else}
-  <a href={href}>{text} ↗</a>
+  <a {href} {target}>{text}</a>
 {/if}
+
+<style>
+  a {
+    padding: 0;
+    font-weight: inherit;
+    text-decoration: none;
+    color: var(--color-dark);
+    font-size: var(--font-size-regular);
+    border-bottom: 1px solid var(--color-dark);
+  }
+
+  a:hover {
+    border-bottom: 1px solid var(--color-orange);
+  }
+</style>
