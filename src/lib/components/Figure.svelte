2c1b3281e52ae16d202da5928400910b06582eea
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-10-27 19:08:12 +0600

    added figure component

---
+++ src/lib/components/Figure.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let classes: string = "";
+</script>
+
+<figure class={classes}>
+  <slot />
+</figure>
+
+<style>
+  figure {
+    margin: 0;
+    padding-bottom: var(--gap-xlarge);
+  }
+</style>
