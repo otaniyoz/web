f493a76f7e727ff44343bc92502c116f1a45fa3f
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-10-27 23:21:53 +0600

    cleaned up pomes

--- src/routes/projects/pomes/+page.svelte
+++ src/routes/projects/pomes/+page.svelte
@@ -10,63 +10,31 @@
 </svelte:head>
 
 <SectionGrid>
-  <article class="narrative">
-    <Heading level={1} useInline={false}>Pomes</Heading>
-    <p>
-      {typographString(
-        "Sometimes I write poems, a term used here quite carelessly I should say, here are some of them, from latest to oldest:"
-      )}
-    </p>
-  </article>
+  <Heading classes="grid-col17" level={1} useInline={false}>Pomes</Heading>
+  <p class="grid-col17">
+    {typographString(
+      "Sometimes I write poems, a term used here quite carelessly I should say, here are some of them, from latest to oldest:"
+    )}
+  </p>
 </SectionGrid>
 {#if pomes}
   {#each pomes as pome}
     {#if pome.published === "true"}
       <SectionGrid>
-        <article class="pome-body">
-          <Heading level={2} useInline={false}>
-            {typographString(pome.title)}</Heading
-          >
-          {#each pome.content as line}
-            <p>{typographString(line)}</p>
-          {/each}
-          <p class="pome-date">{typographString(pome.date)}</p>
-        </article>
+        <Heading classes="centered-visual" level={2} useInline={false}>
+          {typographString(pome.title)}</Heading
+        >
+        {#each pome.content as line}
+          <p class="centered-visual serif">{typographString(line)}</p>
+        {/each}
+        <p class="large-bottom-padding regular-top-padding centered-visual">
+          {typographString(pome.date)}
+        </p>
       </SectionGrid>
     {/if}
   {/each}
 {:else}
   <SectionGrid>
-    <article class="narrative">
-      <p>No poems</p>
-    </article>
+    <p class="grid-col17">No poems</p>
   </SectionGrid>
 {/if}
-
-<style>
-  .narrative {
-    grid-area: auto / 1 / auto / 7;
-  }
-
-  .pome-body {
-    grid-area: auto / 2 / auto / 8;
-    font-family: "IBM Plex Serif";
-    padding-bottom: var(--gap-large);
-  }
-
-  .pome-date {
-    font-family: "IBM Plex Sans";
-    grid-area: auto / 2 / auto / 8;
-    padding-bottom: var(--gap-xlarge);
-  }
-
-  @media screen and (max-width: 1220px) {
-    .pome-body {
-      grid-area: auto / 1 / auto / 7;
-    }
-
-    .pome-date {
-      grid-area: auto / 1 / auto / 7;
-    }
-  }
-</style>
