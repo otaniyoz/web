07e0d323400b76aeb9910fd803bbbd0482ba5178
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-10-27 19:03:53 +0600

    added image component

---
+++ src/lib/components/Image.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let src: string;
+  export let alt: string;
+  export let classes: string = "";
+</script>
+
+<slot name="sup" />
+<img {src} {alt} class={classes} />
+<slot name="sub" />
+
+<style>
+  img {
+    width: 100%;
+    height: 100%;
+    max-width: 100%;
+    background-color: var(--color-light);
+  }
+</style>
