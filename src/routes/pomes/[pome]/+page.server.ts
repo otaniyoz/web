5433c7223e832fb4bd1e89d69b76b34a84401862
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-11-27 10:24:02 +0600

    cleaned up a bit more

--- src/routes/pomes/[pome]/+page.server.ts
+++ src/routes/pomes/[pome]/+page.server.ts
@@ -4,7 +4,8 @@ import type { NoteContentType } from "$lib/types";
 export async function load({ params }): Promise<NoteContentType> {
   try {
     const pome = await import(`../../../data/${params.pome}.pome?raw`);
-    return { post: {title: pome.default.split('\n')[1], content: pome.default.split('\n').slice(2), date: pome.default.split('\n')[0] } };
+    const pomeContent = pome.default.split('\n');
+    return { post: {title: pomeContent[1], content: pomeContent.slice(2), date: pomeContent[0] } };
   } catch {
     throw error(404, 'The pome you tried to access is ' + params.pome + ', but it doesn’t exist');
   }
