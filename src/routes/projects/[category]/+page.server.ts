a2efb5e77f95be6d9ca19da6334f3c29ab64de3b
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-11-28 18:48:03 +0600

    updated projects category adress

--- src/routes/projects/[category]/+page.server.ts
+++ src/routes/projects/[category]/+page.server.ts
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 import fetchProjects from "../../../utils/fetchProjects";
 
 export async function load({ params }) {
-  const fetched = await fetchProjects({category: params.category}); 
+  const fetched = await fetchProjects({category: params.category.replace('-', ' ')}); 
   if (fetched.projects.length) { return fetched; } 
   throw error(404, 'Didn’t find projects under ' + params.category);
 }
