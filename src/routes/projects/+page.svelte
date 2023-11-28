704135c5b50693279e3d3d1e1b65d68fa77df6f2
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2023-11-28 15:44:04 +0600

    i did it again

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Heading from "$lib/components/Heading.svelte";
+</script>
+
+<svelte:head><title>Projects</title></svelte:head>
+
+<Heading level={1}>This page intentionally left blank</Heading>
