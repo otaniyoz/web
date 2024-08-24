7022e75f584a4dfee8857f794658f7635d340ef5
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2024-08-25 02:56:31 +0500

    err

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 window.onload = () => {
-  console.log('loaded 2');
   document.addEventListener('scroll', () => {
     const scrollRotated = document.getElementsByClassName('scroll-rotated');
     for (let rotatedElement of scrollRotated) {
