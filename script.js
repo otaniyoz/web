ebe240170e9f016fa46279fc492643e79d2fc61b
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2024-09-19 11:56:03 +0500

    cleanup

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 'use strict';
 window.onload = () => {
-  function animationInterval(ms, signal, callback) {
-    const start = document.timeline ? document.timeline.currentTime : performance.now();
-    function frame(time) {
-      if (!signal) return;
-      callback(time);
-      scheduleFrame(time);
-    }
-    function scheduleFrame(time) {
-      const elapsed = time - start;
-      const roundedElapsed = Math.round(elapsed / ms) * ms;
-      const targetNext = start + roundedElapsed + ms;
-      const delay = targetNext - performance.now();
-      timer = window.setTimeout(() => { window.requestAnimationFrame(frame); }, delay);
-    }
-    scheduleFrame(start);
-  }
-
   // checks if at least some part of the element is visible. THIS IS NOT EXHAUSTIVE OKAY
   function isInView(element) {
     const rect = element.getBoundingClientRect();
@@ -24,20 +7,11 @@ window.onload = () => {
   }
 
   function animateScroll() {
-    if (scrolling) {
-      const windowPageYOffset = window.pageYOffset;
-      for (let element of scrollRotated) {
-        if (isInView(element)) {
-          element.style.transform = `rotate(${windowPageYOffset}deg)`;
-        }
-      }
-      for (let element of scrollSkewed) {
-        if (isInView(element)) {
-          element.style.transform = `skew(${0.035*windowPageYOffset}deg, ${0.025*windowPageYOffset}deg) translate(-${0.05*windowPageYOffset}px, ${0.02*windowPageYOffset}px)`;
-        }
+    for (let element of scrollRotated) {
+      if (isInView(element)) {
+        element.style.transform = `rotate(${window.pageYOffset}deg)`;
       }
     }
-    scrolling = false;
   }
 
   function animateEyes(event) {
@@ -53,14 +27,9 @@ window.onload = () => {
     insides[1].style.transform = `translate(${dx}px, ${dy}px)`;
   }
 
-  const fps = 60;
-  let timer = null;
   let scrolling = false;
-  let animationController = true;
   const eyes = document.getElementById('eyes');
   const scrollRotated = document.getElementsByClassName('scroll-rotated');
-  const scrollSkewed = document.getElementsByClassName('scroll-skewed');
-  animationInterval(1000 / fps, animationController, animateScroll);
-  window.addEventListener('scroll', () => { scrolling = true; });
+  window.addEventListener('scroll', animateScroll);
   eyes.addEventListener('mousemove', animateEyes);
 };
