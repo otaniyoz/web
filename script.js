1b1478089f0195db7c95f0543df4c6c51959b8dc
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2025-05-16 22:04:41 +0500

    fix font and update tg link

--- script.js
+++ script.js
@@ -6,22 +6,20 @@ window.onload = () => {
   }
 
   function animateScroll() {
-    if (!scrolling.length && !scrollRotated.length) return;
+    if (!parallax.length && !scrollRotated.length) return;
     
-    const scale = 0.2;
+    const scale = 0.15;
     const windowPageYOffset = window.pageYOffset;
-    for (let element of scrolling) {
+    for (const element of parallax) {
       const maxScroll = Math.min((element.height - element.parentElement.offsetHeight) / scale, element.height);
-      if (isInView(element) && (windowPageYOffset > 0 && windowPageYOffset <= maxScroll)) {
-        element.style.top = (-windowPageYOffset * scale) + 'px';
-      }
+      if (isInView(element) && (windowPageYOffset > 0 && windowPageYOffset <= maxScroll)) element.style.top = `${(-windowPageYOffset * scale)}px`;
     }
     for (let element of scrollRotated) {
       if (isInView(element)) element.style.transform = `rotate(${windowPageYOffset}deg)`;
     }
   }
 
-  const scrolling = document.getElementsByClassName('scrolling');
+  const parallax = document.getElementsByClassName('parallax');
   const scrollRotated = document.getElementsByClassName('scroll-rotated');
   document.addEventListener('scroll', animateScroll);
 };
