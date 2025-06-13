257b7711f76beef6f4c706ec8cb0c38d0b979de2
otaniyoz <37662409+otaniyoz@users.noreply.github.com>
2025-06-13 19:26:24 +0500

    add width and height to some images and optimise code

--- projects/pocket-puzzles/script.js
+++ projects/pocket-puzzles/script.js
@@ -1,29 +1,13 @@
 'use strict';
 window.onload = () => {
-  const sound1 = document.getElementById('paper-1');
-  const sound2 = document.getElementById('paper-2');
-  const sound3 = document.getElementById('bubble-wrap');
-  document.getElementById('sound-container-1').addEventListener("mouseenter", () => {
-    sound1.play();
-  });
-  document.getElementById('sound-container-1').addEventListener("mouseleave", function(){
-    sound1.pause();
-    sound1.currentTime = 0;
-  });
+  const soundsContainer = document.getElementById('sounds-container');
+  const sound1 = document.getElementById('sound-1');
+  const sound2 = document.getElementById('sound-2');
+  const sound3 = document.getElementById('sound-3');
 
-  document.getElementById('sound-container-2').addEventListener("mouseenter", () => {
-    sound2.play();
-  });
-  document.getElementById('sound-container-2').addEventListener("mouseleave", function(){
-    sound2.pause();
-    sound2.currentTime = 0;
-  });
-
-  document.getElementById('sound-container-3').addEventListener("mouseenter", () => {
-    sound3.play();
-  });
-  document.getElementById('sound-container-3').addEventListener("mouseleave", function(){
-    sound3.pause();
-    sound3.currentTime = 0;
+  soundsContainer.addEventListener('pointerdown', (event) => {
+    if (event.target.id === 'image-1') sound1.play();
+    else if (event.target.id === 'image-2') sound2.play();
+    else if (event.target.id === 'image-3') sound3.play();
   });
 };
